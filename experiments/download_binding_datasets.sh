8af4068738e46a353ad9a54ce0bf5f185c042a2b
Paul Morris <pmorris2012@fau.edu>
2020-09-15 19:16:20 -0400

    Add files via upload

---
+++ experiments/download_binding_datasets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/mpcrlab/MolecularTransformerEmbeddings/releases/download/binding_datasets/1053197.tsv -O binding_datasets/1053197.tsv
+wget https://github.com/mpcrlab/MolecularTransformerEmbeddings/releases/download/binding_datasets/652067.tsv -O binding_datasets/652067.tsv
+
